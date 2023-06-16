b84a335e30fc2425c7f01fa817d8b141da412055
chaituahire <83580550+chaituahire@users.noreply.github.com>
2023-06-16 12:14:27 +0530

    done

---
+++ arqaam/index.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
+	<script src="assets/js/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<title>Home</title>
+	<link rel="shortcut icon" href="assets/images/favicon.ico">
+</head>
+<body>
+
+<div class="wrapper">
+	<div class="container-fluid main">
+		<div class="header">
+			<div class="row">
+				<nav class="navbar">
+				  <div class="container-fluid col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				    
+				    <ul class="nav navbar-nav navbar-right c-top-nav">
+					      <li class="active" ><h6><a href="#">Home |</h6></a></li>
+					      <li><h6><a href="#" class="">Our Offices |</h6></a></li>
+					      <li ><h6><a href="#">Sitemap</a></h6></li>      
+				    </ul>
+				  </div>
+				</nav>
+			</div>
+			<div class="row">
+			<nav class="navbar">
+			  <div class="container-fluid">
+			    <div class="navbar-header col-xs-10 col-sm-10 col-md-10 col-lg-10 c-top-nav2">
+			      <a class="navbar-brand logo" href="#"><img src="assets/images/logo.png"></a>
+			    </div>
+			    <ul class="nav navbar-nav navbar-right col-xs-2 col-sm-2 col-md-2 col-lg-2">
+			      <li class="pull-right"><a href="#" class="login text-uppercase"><img src="assets/images/login.png"> Login</a></li>
+			    </ul>
+			  </div>
+			</nav>
+			</div>
+			<nav class="navbar">
+			<div class="container-fluid">
+				<div class="row">
+					<ul class="nav navbar-nav  col-sm-12 col-md-12 col-lg-12 c-top-nav3">
+				        <li class="active a"><a href="#" class="ac">Home <span class="sr-only">(current)</span></a></li>
+				        
+				        <li class="dropdown">
+				          <a href="#" class="dropdown-toggle ac" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About Arqaam</a>
+				          <ul class="dropdown-menu">
+				            <li><a href="#">Overview</a></li>
+				            <li><a href="#">Company Corporate Structure</a></li>
+				          </ul>
+				        </li>
+				        <li class="dropdown">
+				          <a href="#" class="dropdown-toggle ac" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Our Services</a>
+				          <ul class="dropdown-menu">
+				            <li><a href="#">Cash Equity</a></li>
+				            <li><a href="#">Corporate Finance</a></li>
+				            <li><a href="#">Asset Management</a></li>
+				            <li><a href="#">Credit Trading</a></li>
+				            <li><a href="#">Equity Derivatives</a></li>
+				            <li><a href="#">Treasury</a></li>
+				            <li><a href="#">Custody</a></li>
+				            <li><a href="#">Research</a></li>
+				          </ul>
+				        </li>
+				        <li class="dropdown">
+				          <a href="#" class="dropdown-toggle ac" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Careers</a>
+				          <ul class="dropdown-menu">
+				            <li><a href="#">Our People</a></li>
+				            <li><a href="#">Why Join Us?</a></li>
+				          </ul>
+				        </li>
+				        <li class="dropdown">
+				          <a href="#" class="dropdown-toggle ac" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Media Centre</a>
+				          <ul class="dropdown-menu">
+				            <li><a href="#">Press Releases</a></li>
+				            <li><a href="#">Media Pack</a></li>
+				            <li><a href="#">Events</a></li>
+				          </ul>
+				        </li>
+				        <li><a href="#" class="ac">Our Offices</a></li>
+				    </ul>
+			    </div>   
+			</div>
+			</nav>
+		</div>
+		
+				
+			<div class="main-slider">
+				<div id="myCarousel" class="carousel slide c-slider" data-ride="carousel">
+					<div class="container mycontainer">
+					    <!-- Indicators -->
+					    <ol class="carousel-indicators ol">
+					      <li data-target="#myCarousel" data-slide-to="0" class="active"><span class="c-style">1</span></li>
+					      <li data-target="#myCarousel" data-slide-to="1"><span class="c-style">2</span></li>
+					      <li data-target="#myCarousel" data-slide-to="2"><span class="c-style">3</span></li>
+					    </ol>
+
+					    <!-- Wrapper for slides -->
+					    <div class="carousel-inner c-inner">
+					      <div class="item active ">
+					      	<div class="inner-box">
+							      <div class="c-slide">
+							        <h3>Sales & Trading</h3>
+							        <p>Unmatched access and seamless execution</p>
+							      </div>
+						     </div>
+					        <img src="assets/images/banner1.jpg" alt="slider-img" class="slider-img img-responsive">
+								
+						   </div>
+					      <div class="item">
+					      		<div class="inner-box">
+							      <div class="c-slide">
+							        <h3>Investment Banking</h3>
+							        <p>Bridging Capital and Opportunity</p>
+							      </div>
+						        </div>
+					        <img src="assets/images/banner2.jpg" alt="slider-img" class="slider-img img-responsive">
+
+
+					      </div>
+					    
+					      <div class="item">
+					      		<div class="inner-box">
+									<div class="c-slide">
+								        <h3>Asset Management</h3>
+								        <p>Performance through in-depth analysis</p>	       
+								      </div>
+							    </div>
+					        <img src="assets/images/banner3.jpg" alt="slider-img" class="slider-img img-responsive">
+
+					      </div>
+					    </div>
+
+					    <!-- Left and right controls -->
+		
+					</div>
+				</div>
+			</div>
+			
+
+				<div class="homepage">
+					<div class="container-fluid">
+						<div class="row">
+							<div class="h-left thumbnail col-xs-4 col-sm-4  col-md-4 col-lg-4">
+								<div class="h-intro">
+									<h2>About Us</h2>
+									<p>Arqaam Capital is the specialist emerging markets investment bank, bringing regional and international product offerings to Emerging Markets.
+										<a href="#">[+] Read More </a>
+									</p>
+								</div>
+								<div class="h-services pull-left">
+									<h3>Service Overview</h3>
+									<ul class="list-group list-unstyled pull-left service-ul">
+										<li><a href="#"> Cash Equity</a></li>
+
+										<li><a href="#"> Corporate Finance</a></li>
+
+										<li><a href="#"> Asset Management</a></li>
+
+										<li><a href="#"> Credit Trading</a></li>
+
+										<li><a href="#"> Equity Derivatives</a></li>
+
+										<li><a href="#" > Treasury</a></li>
+
+										<li><a href="#">Custody</a></li>
+										
+										<li><a href="#"> Research</a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="h-right col-xs-8 col-sm-8 col-md-8 col-lg-8">
+								<ul class="list-group list-unstyled h-right-1">
+									<li class="pull-left"><a href="#"><span class="c-news"><img src="assets/images/icon-news.png"> News</span></a>
+										<span class="empty">
+										</span>
+									</li>
+									<li class="pull-right"><a href="#"><span>Subscribe to news <img src="assets/images/sub.png"></span></a></li>
+								</ul>
+								<div class="newslist">
+									<ul class="list-unstyled news-ul">
+										<li class="news-li li-1">
+										
+											<div class="c-span">
+												<span class="date">25
+													<span class="month">Aug</span>
+												</span>
+											</div>
+											
+										
+											<a href="#">Arqaam managed fund sells its stake in Al Dur Power to Kyushu, Japan.</a>
+											<p class="text-justify">
+												A consortium of GCC financial
+												institutions which includes First Energy Bank, Ibdar Capital,
+												Bahrain Islamic Bank, and Bunyah GCC Infrastructure Fund (managed
+												by Arqaam Capital) have successfully divested their approx. 20%
+												shareholding in Al Dur Power and Water Company (ADPWC) to Kyuden
+												International Corporation (KIC), which is the international
+												business arm of Kyushu Electric Power Company (Kyushu Electric),
+												one of Japan's leading.
+												<a href="#" class="pull-right">[+] Read More </a>
+											</p>
+											
+										</li>
+										<li class="news-li">
+											<div class="c-span">
+												<span class="date">25
+													<span class="month">Aug</span>
+												</span>
+											</div>
+										
+											<a href="#">Arqaam managed fund sells its stake in Al Dur Power to Kyushu, Japan.</a>
+											<p class="text-justify">
+												A consortium of GCC financial
+												institutions which includes First Energy Bank, Ibdar Capital,
+												Bahrain Islamic Bank, and Bunyah GCC Infrastructure Fund (managed
+												by Arqaam Capital) have successfully divested their approx. 20%
+												shareholding in Al Dur Power and Water Company (ADPWC) to Kyuden
+												International Corporation (KIC), which is the international
+												business arm of Kyushu Electric Power Company (Kyushu Electric),
+												one of Japan's leading.
+												<a href="#" class="pull-right">[+] Read More </a>
+											</p>
+											
+										</li>
+										<li class="news-li">
+											<div class="c-span">
+												<span class="date">25
+												<span class="month">Aug</span>
+											</span>
+											</div>
+											<a href="#">Arqaam managed fund sells its stake in Al Dur Power to Kyushu, Japan.</a>
+											<p class="text-justify">
+												A consortium of GCC financial
+												institutions which includes First Energy Bank, Ibdar Capital,
+												Bahrain Islamic Bank, and Bunyah GCC Infrastructure Fund (managed
+												by Arqaam Capital) have successfully divested their approx. 20%
+												shareholding in Al Dur Power and Water Company (ADPWC) to Kyuden
+												International Corporation (KIC), which is the international
+												business arm of Kyushu Electric Power Company (Kyushu Electric),
+												one of Japan's leading.<a href="#" class="pull-right">[+] Read More </a>
+											</p>
+											<p>For further information please click on 
+												<a href="#">Arqaam Investors Conference 2021</a>
+												<li class="c-last-more"><span class="pull-right"><a href="#">More</a></span></li>
+											</p>
+											
+										</li>
+										
+									</ul>
+									
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+		</div>
+	<div class="container-fluid footer">	
+			<ul class="footer-ul pull-left list-unstyled list-inline">
+				<li><a href="#">Terms of use</a></li>
+				<li><a href="#">Privacy</a></li>
+			</ul>
+			<div class="copywrite pull-right">
+				<h6>© 2016 Arqaam Capital | Regulated by DFSA<h6>
+			</div>	
+	</div>
+
+	</div>
+
+
+	<script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
